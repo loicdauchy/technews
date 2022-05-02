7298878571cf9f4085983279f506c830c24eddc0
Antoine Piron <53710517+Whitenor@users.noreply.github.com>
2022-05-02 13:11:16 +0200

    ajout de thumbnail + début travail sur template actualité

--- wp-content/themes/technews/template_actu.php
+++ wp-content/themes/technews/template_actu.php
@@ -20,17 +20,19 @@
   <?= the_content() ?>
 
   <?php if ($the_query->have_posts()) : ?>
-		<?php while ($the_query->have_posts()) : $the_query->the_post(); ?>  
-            
-                <h2><?php the_title(); ?></h2>
-                <?php
-                  if ( has_post_thumbnail() ) { // Vérifies qu'une miniature est associée à l'article.
-                    the_post_thumbnail();
-                  }
-                ?>
-                <p><?= the_excerpt(); ?></p>
-                <a href="<?= get_permalink(get_the_ID()) ?>">voir plus</a>
-                      
+		<?php while ($the_query->have_posts()) : $the_query->the_post(); ?>
+      <div>
+        <?php
+            if ( has_post_thumbnail() ) { // Vérifies qu'une miniature est associée à l'article.
+            the_post_thumbnail('thumbnail', array('class' => 'test'));
+            }
+        ?>
+        <div>
+          <h2><?php the_title(); ?></h2>        
+          <p><?= the_excerpt(); ?></p>
+          <a href="<?= get_permalink(get_the_ID()) ?>">voir plus</a>
+        </div>
+      </div>                
 		<?php endwhile; ?>
 	    <?php wp_reset_postdata(); ?>
     <?php endif; ?>
--- wp-content/themes/technews/template_metiers.php
+++ wp-content/themes/technews/template_metiers.php
@@ -23,32 +23,35 @@
   <?php if ($the_query->have_posts()) : ?>
 		<?php while ($the_query->have_posts()) : $the_query->the_post(); ?>  
               <div>
-                <h2><?php the_title(); ?></h2>
+              <a href="<?= get_permalink(get_the_ID()) ?>">
                 <?php
                   if ( has_post_thumbnail() ) { // Vérifies qu'une miniature est associée à l'article.
-                    the_post_thumbnail();
+                    the_post_thumbnail('thumbnail', array('class' => 'test'));
                   }
-                ?>
-                <p><?= the_excerpt(); ?></p>
-                <a href="<?= get_permalink(get_the_ID()) ?>">voir plus</a>
+                  else{?>
+                  <a href="<?= get_permalink(get_the_ID()) ?>">voir plus</a>
+                  <?php }?>
+                
+                  </a>
+                <h2><?php the_title(); ?></h2>
                 <br><br>
               </div>
                       
-	<?php endwhile; ?>
-  <div id="navigation">
-    <?php 
-    
-      $GLOBALS['wp_query']->max_num_pages = $the_query->max_num_pages;
-      the_posts_pagination( array(
-        'mid_size' => 8,
-        'prev_text' => '<',
-        'next_text' => '>'
-      ));              
+	  <?php endwhile; ?>
+      <div id="navigation">
+      <?php 
+      
+        $GLOBALS['wp_query']->max_num_pages = $the_query->max_num_pages;
+        the_posts_pagination( array(
+          'mid_size' => 8,
+          'prev_text' => '<',
+          'next_text' => '>'
+        ));              
 
-    ?>
-  </div>
+      ?>
+    </div>
 
-    <?php wp_reset_postdata(); ?>
+      <?php wp_reset_postdata(); ?>
   <?php endif; ?>
   
 
