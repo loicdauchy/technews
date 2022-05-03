db8e007f73a5ddbb778fe1cfa3087d1881380f1a
Antoine Piron <53710517+Whitenor@users.noreply.github.com>
2022-05-03 16:15:07 +0200

    update potentiellement final du footer , test du css soon

--- wp-content/themes/technews/footer.php
+++ wp-content/themes/technews/footer.php
@@ -26,14 +26,40 @@
 			<div id="techTitle">
 				<h2>Technews</h2>
 				<p>Lorem Ipsum is simply dummy text of the printing and typesetting industry.<br/> Lorem Ipsum has been the industry's standard dummy text ever since the 1500s</p>
-				<div id="mailDiv">
-					<i class="fa-solid fa-envelope"></i>
-					<a href="<?php the_field('mail') ?>"><?php the_field('mail') ?></a>
+				<div id="contactTech">
+					<a href="mailto:<?php the_field('mail') ?>"><i class="fa-solid fa-envelope"></i><?php the_field('mail') ?></a>
 				</div>
 			</div>
-			<div id="firstBlockRight">
+			<div>
+                <div class="ligneFooter"></div>
+            </div>
 
-			</div>
+            <div class="suivezFooter">
+                <h3>Suivez-nous sur les réseaux !</h3>
+                <div class="reseauFooter">
+					<?php if(is_null(get_field('facebook'))==false) {?>
+						<div class="roundFooter">
+							<a href="<?php the_field('facebook') ?>" class="fa-brands fa-facebook-f" target="_blank"></a>
+						</div>
+					<?php }
+					if(is_null(get_field('twitter'))==false) {?>
+                    <div class="roundFooter">
+                        <a href="<?php the_field('twitter') ?>" class="fa-brands fa-twitter" target="_blank"></a>
+                    </div>
+					<?php }
+					if(is_null(get_field('linkedin'))==false) {?>
+                    <div class="roundFooter">
+                        <a href="<?php the_field('linkedin') ?>" class="fa-brands fa-linkedin" target="_blank"></a>
+                    </div>
+					<?php } 
+					if(is_null(get_field('instagram'))==false) {?>
+                    <div class="roundFooter">
+                        <a href="<?php the_field('instagram') ?>" class="fa-brands fa-instagram" target="_blank"></a>
+                    </div>
+					<?php } ?>
+                </div>
+                <a class="politique" href="">Politique de Confidentialité</a>
+            </div>
 		</div>
 		<div id="SecondBlock">
 
