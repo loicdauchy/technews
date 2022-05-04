76276cacfb319c39d32d4b02c6a1ac037730c7bf
Brocard Bernard <99798844+Bernard39600@users.noreply.github.com>
2022-05-04 15:30:25 +0200

    css page metier

--- wp-content/themes/technews/single-metier.php
+++ wp-content/themes/technews/single-metier.php
@@ -2,8 +2,8 @@
 <?php get_header(); ?>
 
 <div id="main">
-  <h2><?= the_title() ?></h2>
-  <p><?= the_content() ?></p>
+  <h2 class="h2metier"><?= the_title() ?></h2>
+  <p class="pmetier"><?= the_content() ?></p>
   <!-- <a href="<?php the_field('lien_fiche_metier') ?>" target="_blank">Lien vers la fiche métier</a> -->
 </div>
 
