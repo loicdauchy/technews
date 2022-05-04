17b3156b8a5f098bb6299ddf953cfd54249133b2
NatanD <natandav39@gmail.com>
2022-05-04 13:34:37 +0200

    End

---
+++ wp-content/themes/technews/assets/js/app.js
@@ -0,0 +1,21 @@
+var content = document.querySelector('#Nhamburger-content');
+var sidebarBody = document.querySelector('#Nhamburger-sidebar-body');
+var button = document.querySelector('#Nhamburger-button');
+var buttonclose = document.querySelector('#Nhamburger-button-close');
+var overlay = document.querySelector('#Nhamburger-overlay');
+var activatedClass = 'Nhamburger-activated'
+
+sidebarBody.innerHTML = content.innerHTML
+
+button.addEventListener('click', function (e) {
+    e.preventDefault();
+
+    this.parentNode.classList.add(activatedClass);
+});
+
+buttonclose.addEventListener('click', function (e) {
+    e.preventDefault();
+
+    // this.parentNode.classList.remove(activatedClass);
+    document.getElementById('Nhamburger').classList.remove(activatedClass);
+});
