5d03da0d88212842e61cf513207798f6db544ddd
Antoine Piron <53710517+Whitenor@users.noreply.github.com>
2022-05-04 10:38:03 +0200

    update single template métier

--- wp-content/themes/technews/single-metier.php
+++ wp-content/themes/technews/single-metier.php
@@ -2,7 +2,7 @@
 <?php get_header(); ?>
 
 <div id="main">
-  <h2><?= the_title() ?></h2>
+  <h2 id="h2metier"><?= the_title() ?></h2>
   <p><?= the_content() ?></p>
   <a href="<?php the_field('lien_fiche_metier') ?>" target="_blank">Lien vers la fiche métier</a>
 </div>
