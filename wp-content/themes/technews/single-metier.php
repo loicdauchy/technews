0ea1d276213cbd4c0ec7f5d753ccee62f7ff6673
Brocard Bernard <99798844+Bernard39600@users.noreply.github.com>
2022-05-04 16:37:37 +0200

    maj metiers

--- wp-content/themes/technews/single-metier.php
+++ wp-content/themes/technews/single-metier.php
@@ -2,8 +2,14 @@
 <?php get_header(); ?>
 
 <div id="main">
+  <div>
   <h2 class="h2metier"><?= the_title() ?></h2>
-  <p class="pmetier"><?= the_content() ?></p>
+  </div>
+
+  <div class='metierContent'>
+  <?= the_content() ?>
+  </div>
+
   <!-- <a href="<?php the_field('lien_fiche_metier') ?>" target="_blank">Lien vers la fiche métier</a> -->
 </div>
 
