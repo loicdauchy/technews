1c100fa05f870bffaa044eee204c002e9b2abae0
Antoine Piron <53710517+Whitenor@users.noreply.github.com>
2022-05-04 19:41:51 +0200

    démarrage mise en page témoignage

--- wp-content/themes/technews/template_temoignage.php
+++ wp-content/themes/technews/template_temoignage.php
@@ -15,43 +15,50 @@
   $the_query = new WP_Query($args);
 
 ?>
-<div id="main">
-<h1>Les témoignages</h1>
-  <?= the_content() ?>
-
+<div class="Ntemoin">
+  <h1>Les témoignages</h1>
+  <div class="NreseauNav">
+    <a href="#" class="fa-brands fa-facebook-f"></a>
+    <a href="#" class="fa-brands fa-twitter"></a>
+    <a href="#" class="fa-brands fa-linkedin"></a>
+    <a href="#" class="fa-brands fa-instagram"></a>
+  </div>
+</div>
+<section class="Nsectiontem">
   <?php if ($the_query->have_posts()) : ?>
-		<?php while ($the_query->have_posts()) : $the_query->the_post(); ?>  
-                <div class="temoingnage">
-                  <?php
+  <?php while ($the_query->have_posts()) : $the_query->the_post(); ?>
+
+  <?php
                     if ( has_post_thumbnail() ) {
                       the_post_thumbnail('thumbnail', array('class' => 'thumbnailTemoin'));
                     }
                   ?>
-                  <div>
-                    <h2><?php the_title(); ?></h2>
-                    <p><?= the_content(); ?></p>
-                    <div>
-                    <?php
+  <div>
+    <h2><?php the_title(); ?></h2>
+    <p><?= the_content(); ?></p>
+    <div>
+      <?php
                     if ( get_field('lien_temoignage_video')!= '' ) {
                         ?>
-                      <a href="<?php the_field('lien_temoignage_video') ?>" target='_blank' class='temoinOrange'>Témoignage Vidéo</a>
-                    <?php
+      <a href="<?php the_field('lien_temoignage_video') ?>" target='_blank' class='temoinOrange'>Témoignage Vidéo</a>
+      <?php
                       }
                     ?>
-                      <a href="../metier/<?php the_field('metier_liee') ?>" class='temoinWhite'> Métier associé</a>
-                    <?php
+      <a href="../metier/<?php the_field('metier_liee') ?>" class='temoinWhite'> Métier associé</a>
+      <?php
                     if ( get_field('metier_liee_bis')!='' ) {
                     ?>
-                      <a href="../metier/<?php the_field('metier_liee_bis') ?>" target='_blank' class='temoinWhite'>Second métier associé</a>
-                    <?php
+      <a href="../metier/<?php the_field('metier_liee_bis') ?>" target='_blank' class='temoinWhite'>Second métier
+        associé</a>
+      <?php
                       }
                     ?>
-                    </div>
-                  </div>
-                </div>
-		<?php endwhile; ?>
-    <div id="navigation">
-      <?php 
+    </div>
+  </div>
+</section>
+<?php endwhile; ?>
+<div id="navigation">
+  <?php 
       
         $GLOBALS['wp_query']->max_num_pages = $the_query->max_num_pages;
         the_posts_pagination( array(
@@ -61,8 +68,7 @@
         ));              
 
       ?>
-    </div>
-	    <?php wp_reset_postdata(); ?>
-    <?php endif; ?>
 </div>
+<?php wp_reset_postdata(); ?>
+<?php endif; ?>
 <?php get_footer(); ?>
