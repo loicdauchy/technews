b759da84e1f109f2ab4418e4db5e12919d7c694b
Jorge Gil Rivas <jorged1509@gmail.com>
2022-05-04 15:58:29 +0200

    Page Actualité updated

--- wp-content/themes/technews/template_actu.php
+++ wp-content/themes/technews/template_actu.php
@@ -16,28 +16,49 @@
 <?php get_header(); ?>
 
 <div id="actupage">
-  <h1><?= get_the_title() ?></h1>
-  <?= the_content() ?>
+  <div class='actuTitle'>
+    <h1><?= get_the_title() ?></h1>
+  </div>
+
 
   <?php if ($the_query->have_posts()) : ?>
 		<?php while ($the_query->have_posts()) : $the_query->the_post(); ?>  
 
 
+    <section id="actualite">
 
-    <h2><?php the_title(); ?></h2>
-    <p><?= the_excerpt(); ?></p>
-    <a href="<?= get_permalink(get_the_ID()) ?>">voir plus</a>
+    <div class='article'>
+      <div class='articleTitle'>
+        <h2><?php the_title(); ?></h2>
+      </div>
+      <div class='articleDate'>
+        <p>Le <?= get_the_date(); ?> à <?= get_the_time(); ?></p>
+      </div>
 
-    <div>
-      <?php 
-        if ( has_post_thumbnail() ) {
-          the_post_thumbnail('thumbnail');
-      }?>
+      <div class='articleImg'>
+        <?php 
+          if ( has_post_thumbnail() ) {
+            the_post_thumbnail('medium_large');
+        }?>
+      </div>
+
+
+      <div class='articleContent'>
+        <p><?= the_excerpt(); ?></p>
+      </div>
+
+
+      <button class='articleButton'  onclick="window.location.href='<?php echo get_permalink(get_the_ID()) ?>'"> 
+        Voir plus 
+      </button>
+      
+      
     </div>
-                      
+    </section>            
 		<?php endwhile; ?>
 	    <?php wp_reset_postdata(); ?>
     <?php endif; ?>
+    
 </div>
 
 <?php get_footer(); ?>
